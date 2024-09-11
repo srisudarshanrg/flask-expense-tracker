f5fe901ed82a931db8d79e46e028418938ff62cf
R Sri Sudarshan <srisudarshanrg@gmail.com>
2024-09-11 09:54:02 +0530

    login and register completed

--- expense_tracker/forms.py
+++ expense_tracker/forms.py
@@ -1,16 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import validators, StringField, PasswordField, SubmitField, EmailField, DateTimeField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms import validators, StringField, PasswordField, SubmitField, DateTimeField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from expense_tracker.models import User
 
 class LoginForm(FlaskForm):
-    credential = StringField(label="Username or Email", validators=[DataRequired()])
+    username = StringField(label="Username", validators=[DataRequired()])
     password = PasswordField(label="Password", validators=[DataRequired()])
     submit = SubmitField(label="Submit")
 
 class RegisterForm(FlaskForm):
+    def validate_username(self, username_attempted):
+        check_exists_user = User.query.filter_by(username=username_attempted.data).all()
+        if len(check_exists_user) > 0 :
+            raise ValidationError(message="This username already exists.")
+
     username = StringField(label="Username", validators=[DataRequired(), Length(min=3, max=30)])
     password = PasswordField(label="Password", validators=[DataRequired()])
     password_confirm = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo(fieldname="password")])
-    dob = DateTimeField(label="Date of Birth", validators=[DataRequired()])
+    dob = StringField(label="Date of Birth", validators=[DataRequired()])
     submit = SubmitField(label="Submit")
 
--- expense_tracker/routes.py
+++ expense_tracker/routes.py
@@ -21,21 +21,16 @@ def profile():
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        credential_entered = login_form.credential.data
+        username_entered = login_form.username.data
         password_entered = login_form.password.data
 
-        attempted_user_username = User.query.filter_by(username=credential_entered).first()
-        attempted_user_email = User.query.filter_by(email=credential_entered).first()
-        if attempted_user_username and CheckPasswordHash(attempted_user_username.password, bytes(password_entered, "utf-8")):
-            login_user(attempted_user_username)
-            flash(message="You have been logged in successfully", category="info")
-            return redirect(url_for("expense_tracker"))
-        elif attempted_user_email and CheckPasswordHash(attempted_user_email.password, bytes(password_entered, "utf-8")):
-            login_user(attempted_user_email)
-            flash(message="You have been logged in successfully", category="info")        
+        attempted_user = User.query.filter_by(username=username_entered).first()
+        if attempted_user and CheckPasswordHash(attempted_user.password, bytes(password_entered, "utf-8")):
+            login_user(attempted_user)
+            flash(message="You have been logged in successfully", category="success")
             return redirect(url_for("expense_tracker"))
         else:
-            flash(message="Incorrect Credentials", category="danger")
+            flash(message="Incorrect credentials", category="danger")
 
     return render_template("login.html", form=login_form)
 
@@ -43,31 +38,32 @@ def login():
 def register():
     register_form = RegisterForm()
 
-    if register_form.validate_on_submit():
+    if register_form.validate_on_submit() and register_form.errors == {}:
         username_entered = register_form.username.data
+        password_entered = register_form.password.data
+        password_hash = HashPassword(password_entered).encode("utf-8")
+        dob_entered = register_form.dob.data
 
-        check_username = User.query.filter_by(username=username_entered).all()
+        current_date = datetime.datetime.now()
+        join_date = current_date.strftime("%d-%m-%Y")
 
-        if len(check_username) > 0:
-            flash(message="This username already exists", category="danger")
-        else:
-            current_date = datetime.datetime.now()
-            current_date_formatted = current_date.strftime("%d-%m-%Y")
-            hashed_password = HashPassword(password=register_form.password.data)
-            new_user = User(
-                username=username_entered,
-                password=hashed_password,
-                dob=register_form.dob.data,
-                join_date=current_date_formatted,
-            )
+        new_user = User(
+            username=username_entered,
+            password=password_hash,
+            # dob=datetime.datetime.strptime(dob_entered),
+            dob=dob_entered,
+            join_date=join_date,
+        )
 
-            db.session.add(new_user)
-            db.session.commit()
+        db.session.add(new_user)
+        db.session.commit()
 
-            login_user(new_user)
+        login_user(new_user)
+        print(current_user)
 
-            flash(message="You have been registered and logged in successfully", category="info")
-
-            return redirect(url_for("expense_tracker"))
+        return redirect(url_for("expense_tracker"))
+    else:
+        for error in register_form.errors.values():
+            flash(message=f"{error}", category="danger")
 
     return render_template("register.html", form=register_form)
